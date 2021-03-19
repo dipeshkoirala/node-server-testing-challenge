9ef4088ede5e861b1fa2b66a1ffb03a3c42be0b9
dipeshkoirala <dipeshkoirala@gmail.com>
2021-03-18 20:23:34 -0400

    Checking data and type through consoleLog

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,9 +1,14 @@
 const supertest=require("supertest")
+const  server=require("../server")
 
 test("Testing the testfile", ()=>{
     expect(10-5).toBeGreaterThan(4)
 })
 
-test("[GET::/", ()=>{
+test("[GET::/",  async ()=>{
+const res= await supertest(server).get("/")
 
+//console.log(res)
+expect(res.status).toBe(200)
+expect(res.text).toBe(/Welcome to our API/i)
 })
